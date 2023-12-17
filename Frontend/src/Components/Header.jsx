55432138e9ac065d008eb0a447ddc04628a7a068
Naaveen kushwaha <naveenkushwaha000@gmail.com>
2023-12-18 01:35:50 +0530

    frontend

---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Header.css'
+function Header() {
+    return (
+        <div className="yogaHeader">
+            <h>Welcome to Flexmoney Yoga Classes </h>
+        </div>
+    )
+}
+
+export default Header
